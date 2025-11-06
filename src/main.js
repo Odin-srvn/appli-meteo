b6a116bd2d4eb46b9bc14320280a513e6b3039f4
Thomas Coment <thomas.coment@gmail.com>
2025-11-06 11:20:07 +0100

    feat:ajout header, footer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import layout from "./views/layout.vue";
 import '@/scss/style.scss';
 
-import '@/style.css';
+//import '@/style.css';
 
 
 createApp(App).mount("#app");
-createApp(layout).mount("#layout");
