35f773470cadea41644ea78db00de4f17b9ca7e3
adam rodrigues <adamrodrigues2001@gmail.com>
2025-11-06 15:41:32 +0100

    fond ville

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import { fetchMeteo } from "./services/meteo.js";
 
 
 const villes = [
-  { code: "bordeaux", nom: "Bordeaux" },
-  { code: "arcachon", nom: "Arcachon" },
-  { code: "libourne", nom: "Libourne" },
-  { code: "blaye", nom: "Blaye" },
-  { code: "langon", nom: "Langon" }
+  { code: "bordeaux", nom: "Bordeaux", image: "/img/bordeaux_banner.jpg" },
+  { code: "arcachon", nom: "Arcachon", image: "/img/arcachon_banner.jpg"},
+  { code: "libourne", nom: "Libourne", image: "/img/libourne_banner.webp" },
+  { code: "blaye", nom: "Blaye", image: "/img/blaye_banner.webp"  },
 ];
+
+const imageVille = computed(() => {
+  const current = villes.find(v => v.code === ville.value);
+  return current ? current.image : "/img/default_banner.jpg";
+});
 
 const ville = ref("bordeaux");
 const meteo = ref(null);
@@ -42,7 +46,11 @@ watch(ville, chargerMeteo)
 <main id="main">
 
   <div class="app-container">
-    <div class="banner">
+    <div class="banner"
+    :class="ville"
+    :style="{
+    backgroundImage: `url(${imageVille})`,
+  }">
         <h1>
             <span class="title">{{ ville.charAt(0).toUpperCase() + ville.slice(1) }}</span>
         </h1>
