5675fa17718a270b7999cc557b13f5134372654a
Odin-srvn <odin.douteaux@hotmail.fr>
2025-11-06 11:13:42 +0100

    add font support, and some classes on the view. edit card style

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@ async function chargerMeteo() {
           <div v-for="j in meteo.days" :key="j.day_long" class="day-card">
               <h5>{{ j.day_long }}</h5>
               <img :src="j.icon" :alt="j.condition" width="64" height="64" />
-              <p>{{ j.tmin }}°C / {{ j.tmax }}°C</p>
+              <p class="temp">{{ j.tmin }}°C / {{ j.tmax }}°C</p>
              <p class="condition">{{ j.condition }}</p>
           </div>
         </section>
