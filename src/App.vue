80c9b15d947bbcc1a14f59992a1c67263fbbe1c7
Thomas Coment <thomas.coment@gmail.com>
2025-11-06 14:19:05 +0100

    ajout onMounted

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, onMounted } from "vue";
 import { fetchMeteo } from "./services/meteo.js";
 
 
@@ -16,83 +16,85 @@ const meteo = ref(null);
 const loading = ref(false);
 const error = ref("");
 
-async function chargerMeteo() {
-  error.value = "";
-  loading.value = true;
-  meteo.value = null;
-  try {
-    meteo.value = await fetchMeteo(ville.value);
-  } catch (e) {
-    error.value = e.message;
-  } finally {
-    loading.value = false;
+onMounted(async () => {
+  async function chargerMeteo() {
+    error.value = "";
+    loading.value = true;
+    meteo.value = null;
+    try {
+      meteo.value = await fetchMeteo(ville.value);
+    } catch (e) {
+      error.value = e.message;
+    } finally {
+      loading.value = false;
+    }
   }
-}
+  chargerMeteo();
+  watch(ville, chargerMeteo);
+})
 
-chargerMeteo();
-watch(ville, chargerMeteo);
 </script>
 
 <template>
   <header id="header">
-            <div class="logo">
-                <a href="#"><img src="/img/logo_climato.webp" class="logo-header" alt=""></a>
-            </div>
-        </header>
-
-<main id="main">
-
-  <div class="app-container">
-    <div class="banner">
-        <h1>
-            <span class="title">Bordeaux</span>
-        </h1>
+    <div class="logo">
+      <a href="#"><img src="/img/logo_climato.webp" class="logo-header" alt=""></a>
     </div>
-    
+  </header>
 
-    <form class="meteo-form" @submit.prevent>
-      <label for="ville">Choisir une ville </label>
-      <div class="form-group">
-        <select id="ville" v-model="ville">
-          <option v-for="v in villes" :key="v.code" :value="v.code">{{ v.nom }}</option>
-        </select>
-        
+  <main id="main">
+
+    <div class="app-container">
+      <div class="banner">
+        <h1>
+          <span class="title">{{ ville.charAt(0).toUpperCase() + ville.slice(1) }}</span>
+        </h1>
       </div>
-    </form>
 
-    <div class="">
-      <div v-if="loading" class="alert info"> Chargement...</div>
-      <div v-else-if="error" class="alert error"> Erreur : {{ error }}</div>
-      
-      <div v-else-if="meteo">
-        <div class="current">
-          <h2 class="city-banner">{{ meteo.city }}</h2>
-          <p>{{ meteo.current.condition }} — {{ meteo.current.tmp }}°C</p>
-          <img :src="meteo.current.icon" alt="meteo actuelle" width="64" height="64" />
+
+      <form class="meteo-form" @submit.prevent>
+        <label for="ville">Choisir une ville </label>
+        <div class="form-group">
+          <select id="ville" v-model="ville">
+            <option v-for="v in villes" :key="v.code" :value="v.code">{{ v.nom }}</option>
+          </select>
+
         </div>
-        
-        <section class="forecast">
-          <div v-for="j in meteo.days" :key="j.day_long" class="day-card">
+      </form>
+
+      <div class="">
+        <div v-if="loading" class="alert info"> Chargement...</div>
+        <div v-else-if="error" class="alert error"> Erreur : {{ error }}</div>
+
+        <div v-else-if="meteo">
+          <div class="current">
+            <h2 class="city-banner">{{ meteo.city }}</h2>
+            <p>{{ meteo.current.condition }} — {{ meteo.current.tmp }}°C</p>
+            <img :src="meteo.current.icon" alt="meteo actuelle" width="64" height="64" />
+          </div>
+
+          <section class="forecast">
+            <div v-for="j in meteo.days" :key="j.day_long" class="day-card">
               <h5>{{ j.day_long }}</h5>
               <img :src="j.icon" :alt="j.condition" width="64" height="64" />
               <p class="temp">{{ j.tmin }}°C / {{ j.tmax }}°C</p>
-             <p class="condition">{{ j.condition }}</p>
-          </div>
-        </section>
+              <p class="condition">{{ j.condition }}</p>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
-  </div>
-</main>
-  
+  </main>
+
   <footer id="footer">
-      <div class="liste">
-        <ul>
-          <li><a href="">A Propos</a></li>
-          <li><a href="">Mentions Légales</a></li>
-        </ul>
-      </div>
-      <div class="logo-bas">
-        <a href="#"><img class="logo-footer" src="/public/img/logo_climato.webp" alt=""></a>
-      </div>
+    <div class="liste">
+      <ul>
+        <li><a href="">A Propos</a></li>
+        <li><a href="">Mentions Légales</a></li>
+      </ul>
+    </div>
+    <div class="logo-bas">
+      <a href="#"><img class="logo-footer" src="/public/img/logo_climato.webp" alt=""></a>
+    </div>
   </footer>
 </template>
